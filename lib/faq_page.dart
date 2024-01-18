1638a35f785d30a084dc4882451ce5d9e8fee020
zannatulzerin <95172922+zannatulzerin@users.noreply.github.com>
2024-01-18 10:21:49 +0600

    FAQ screen fixed

--- lib/faq_page.dart
+++ lib/faq_page.dart
@@ -44,37 +44,27 @@ class FAQPage extends StatelessWidget {
             FAQItem(
               question: 'What is the purpose of the app?',
               answer:
-              'The app is designed to identify objects and colors in real-time using the camera of your Android device.',
+                  'The app is designed to identify objects and colors in real-time using the camera of your Android device.',
             ),
             FAQItem(
               question: 'How does the app work?',
               answer:
-              'From homepage, user can scroll through different portions such as detect objects from in real-time, detect objects from objects, detect texts from images, detect colors from images. User can also use voice assistant to run the features. ',
+                  'From homepage, user can scroll through different portions such as detect objects from in real-time, detect objects from objects, detect texts from images, detect colors from images. User can also use voice assistant to run the features. ',
             ),
             FAQItem(
               question: 'What types of objects can the app detect?',
               answer:
-              'The app can detect a wide range of objects, including everyday items, products, fruits, vehicles, groceries etc.',
-            ),
-            FAQItem(
-              question: 'What types of color can the app identify?',
-              answer:
-              'The app can identify a wide spectrum of colors from images',
+                  'The app can detect a wide range of objects, including everyday items, products, fruits, vehicles, groceries etc and some colors.',
             ),
             FAQItem(
               question: 'What permissions does the app require?',
               answer:
-              'The app requires camera and voice permissions to access the device camera for real-time object and color detection. It also requires internet connection.',
-            ),
-            FAQItem(
-              question: 'What permissions does the app require?',
-              answer:
-              'The app requires camera and voice permissions to access the device camera for real-time object and color detection. It also requires internet connection.',
+                  'The app requires camera and voice permissions to access the device camera for real-time object and color detection. It also requires internet connection.',
             ),
             FAQItem(
               question: 'Can the app translate objects and texts? ',
               answer:
-              'Yes, the app has the capability to recognize and translate objects and texts present in images.',
+                  'Yes, the app has the capability to recognize and translate objects and texts present in images.',
             ),
             // Add more FAQItem widgets as needed
           ],
